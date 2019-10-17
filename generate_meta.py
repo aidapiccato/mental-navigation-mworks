ea70056ca40ec9028cead99556715f637f761202
Aida Piccato <apiccato@dhcp-18-20-129-117.dyn.mit.edu>
2019-10-17 17:12:56 -0400

    fixing

--- generate_meta.py
+++ generate_meta.py
@@ -1,4 +1,4 @@
-# import pickle
+import pickle
 import numpy as np
 
 def generate_meta(n_condition_repeats, num_stims, max_dist, prob, filename):
@@ -10,6 +10,7 @@ def generate_meta(n_condition_repeats, num_stims, max_dist, prob, filename):
     :return: Nothing. Save s
     '''
     meta = {
+        'stim_drift_direction': [],
         'stim_dist_cum': [],
         'meta_index': [],
         'start_stim_index': [],
@@ -22,15 +23,17 @@ def generate_meta(n_condition_repeats, num_stims, max_dist, prob, filename):
     for t in range(n_condition_repeats):
         for pair_index in range(num_pairs):
             meta_index = t * num_pairs + pair_index
+            stim_drift_direction = 1 if np.random.rand() > 0.5 else -1
             meta['start_stim_index'].append(start_idx[pair_index])
             meta['end_stim_index'].append(end_idx[pair_index])
             meta['pair_index'].append(pair_index)
             meta['meta_index'].append(meta_index)
+            meta['stim_drift_direction'].append(stim_drift_direction)
             stim_dist = np.random.geometric(prob, size=num_stims)
             stim_dist = np.clip(stim_dist, 1, max_dist)
             stim_dist[0] = 0
             stim_dist_cum = np.cumsum(stim_dist)
             meta['stim_dist_cum'].append(stim_dist_cum)
 
-    with open(filename, 'ab') as f:
+    with open(filename, 'wb') as f:
         pickle.dump(meta, f)
