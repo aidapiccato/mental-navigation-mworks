3a7e9a9d4887ab5e1f4c6c883e59b26a838a98d9
Aida Piccato <apiccato@dhcp-18-20-160-236.dyn.mit.edu>
2019-12-07 11:41:39 -0500

    configuring session stuff

--- generate_meta.py
+++ generate_meta.py
@@ -1,23 +1,113 @@
 import pickle
 import numpy as np
+from itertools import permutations
+import os
+MAX_NUM_STIMS = 6
+MIN_NUM_STIMS = 2
+N_SEQ_LEN_REPEATS = 2
+IMAGE_PATH = '/images/objects1'
+dir_path = "/Users/apiccato/PycharmProjects/mental-navigation-mworks/pickle_meta"
+
+def length_to_ids(l, n):
+    '''
+    Returns list of n seq_ids, each corr. to sequences of length l
+    :param l: Length of desired seqs
+    :param n: Number of seqs of length l
+    :return:
+    '''
+    if (l == 2):
+        start_id = 0
+    else:
+        start_id = np.math.factorial(l - 1)
+    n_distinct = np.math.factorial(l)
+    ids = np.random.choice(np.arange(n_distinct), n, replace=False)
+    ids += start_id
+    return ids
 
 
-def generate_meta_block(min_num_images=2, max_num_images=6, num_images_repeats=5, image_folder_path='/images/objects1'):
-    meta = {
-        'num_images': [],
-        'image_paths': [],
-        'block_meta_index': []
+def generate_sess_meta(subject_id, meta_fn_tag):
+    ''''
+
+    '''
+    subject = {}
+    subject['seq_index'] = 0
+    # generate set of 10 sequences, two for each length
+    seqs = []
+    for l in np.arange(MIN_NUM_STIMS, MAX_NUM_STIMS + 1):
+        seqs.append(length_to_ids(l, n=N_SEQ_LEN_REPEATS))
+
+    seqs = np.asarray(seqs).reshape((N_SEQ_LEN_REPEATS * (MAX_NUM_STIMS - MIN_NUM_STIMS + 1), ))
+    np.random.shuffle(seqs)
+    subject['seqs'] = seqs
+    fn = "%s/sess_%s.pkl" % (dir_path, meta_fn_tag)
+    d = {}
+    if not os.path.isfile(fn):
+        with open(fn, 'wb') as f:
+            pickle.dump(d, f)
+    with open(fn, 'rb') as f:
+        try:
+            d = pickle.load(f)
+        except (EOFError) as e:
+            d = {}
+
+    d[subject_id] = subject
+
+    with open(fn, 'wb') as f:
+        pickle.dump(d, f)
+
+
+
+def generate_meta(
+        meta_fn_tag,
+        # experiment-level parameters
+        min_num_stims=MIN_NUM_STIMS,
+        max_num_stims=MAX_NUM_STIMS,
+        image_folder_path=IMAGE_PATH,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              # trial-level parameters
+        prob=0.7,
+        max_dist=None,
+        fixed_dist=1,
+        num_options=2
+        ):
+
+    '''
+    Generates meta file with sequences, num_images_repeats of each length in the range (min_num_images). Each
+    :param min_num_stims:
+    :param max_num_stims:
+    :param image_folder_path:
+    :return:
+    '''
+
+    seq_meta = {
+        'n_stims': [],
+        'stim_paths': [],
+        'seq_id': []
     }
-    for num_images_idx, num_images in enumerate(range(min_num_images, max_num_images + 1)):
-        for repeat_idx in range(num_images_repeats):
-            meta['block_meta_index'].append(repeat_idx + num_images_idx * (max_num_images - min_num_images))
-            meta['num_images'].append(num_images)
-            image_ixs = np.random.permutation(np.arange(num_images))
-            image_paths = np.asarray(['%s/%s' % (image_folder_path, image_ix) for image_ix in image_ixs], dtype=str)
-            meta['image_paths'].append(image_paths)
 
-    return meta
-def generate_meta(n_condition_repeats, num_stims, max_dist, prob, filename, num_options_p=None, fixed_dist=None):
+    seq_id = 0
+
+    seq_trial_meta = []
+
+    for num_stims_idx, n_stims in enumerate(range(min_num_stims, max_num_stims + 1)):
+        seq_perms = np.asarray(list(permutations(np.arange(n_stims))))
+        for seq in seq_perms:
+            seq_meta['seq_id'].append(seq_id)
+            seq_meta['n_stims'].append(n_stims)
+            stim_paths = np.asarray(['%s/%s' % (image_folder_path, stim_ix) for stim_ix in seq], dtype=str)
+            seq_meta['stim_paths'].append(stim_paths)
+            trial_meta = generate_trial_meta(n_stims, max_dist, prob, num_options, fixed_dist)
+            seq_trial_meta.append(trial_meta)
+            seq_id += 1
+    trial_meta_fn = "%s/trial_%s.pkl" % (dir_path, meta_fn_tag)
+    with open(trial_meta_fn, 'wb') as f:
+        pickle.dump(seq_trial_meta, f)
+    seq_meta_fn = "%s/seq_%s.pkl" % (dir_path, meta_fn_tag)
+    with open(seq_meta_fn, 'wb') as f:
+        pickle.dump(seq_meta, f)
+
+    return seq_meta
+
+def generate_trial_meta(num_stims, max_dist, prob, num_options, fixed_dist):
     '''
     :param n_condition_repeats: Number of times a pair is repeated (each time with different inter image dist)
     :param n_stims: Number of distinct stimuli
@@ -25,53 +115,44 @@ def generate_meta(n_condition_repeats, num_stims, max_dist, prob, filename, num_
     :param prob: Probability for geometric distribution
     :return: Nothing.
     '''
-    meta = {
+    trial_meta = {
         'stim_drift_direction': [],
         'stim_dist_cum': [],
-        'meta_index': [],
+        'trial_meta_index': [],
         'start_stim_index': [],
         'end_stim_index': [],
-        'pair_index': [],
         'options_bin': [],
         'options_pos': [],
         'num_options': []
     }
-    start_idx = np.repeat(np.linspace(0, num_stims - 1, num=num_stims), num_stims)
-    end_idx = np.tile(np.linspace(0, num_stims - 1, num=num_stims), num_stims)
-    num_pairs = num_stims**2
-    for t in range(n_condition_repeats):
-        for pair_index in range(num_pairs):
-            meta_index = t * num_pairs + pair_index
-            stim_drift_direction = 1 if np.random.rand() > 0.5 else -1
-            meta['start_stim_index'].append(start_idx[pair_index])
-            meta['end_stim_index'].append(end_idx[pair_index])
-            meta['pair_index'].append(pair_index)
-            meta['meta_index'].append(meta_index)
-            meta['stim_drift_direction'].append(stim_drift_direction)
-            end_stim_index = end_idx[pair_index]
-            other = np.arange(num_stims)
-            other = other[np.where(other != end_stim_index)]
-            if num_options_p is None:
-                num_options = np.random.choice([2, 4, num_stims])
-            else:
-                num_options = num_options_p
-            options_ixs = np.asarray(np.concatenate(([end_stim_index],
-                np.random.choice(other, num_options - 1, replace=False))), dtype=int)
-            options_pos, options_bin = np.zeros(num_stims), np.zeros(num_stims)
-            options_bin[options_ixs] = 1
-            options_pos[options_ixs] = np.random.permutation(np.arange(num_options))
-            meta['num_options'].append(num_options)
-            meta['options_pos'].append(options_pos)
-            meta['options_bin'].append(options_bin)
-            if fixed_dist is None:
-                stim_dist = np.random.geometric(prob, size=num_stims)
-                stim_dist = np.clip(stim_dist, 1, max_dist)
-                stim_dist_cum = np.cumsum(stim_dist)
-                meta['stim_dist_cum'].append(stim_dist_cum)
-            else:
-                stim_dist = np.repeat([fixed_dist], num_stims)
-                stim_dist_cum = np.cumsum(stim_dist)
-                meta['stim_dist_cum'].append(stim_dist_cum)
-
-    with open(filename, 'wb') as f:
-        pickle.dump(meta, f)
+    start_idx = np.repeat(np.linspace(0, num_stims - 1, num=num_stims), num_stims*2)
+    end_idx = np.tile(np.linspace(0, num_stims - 1, num=num_stims), num_stims*2)
+    num_pairs = num_stims**2 * 2
+    for pair_index in range(num_pairs):
+        stim_drift_direction = 1 if pair_index < (num_pairs / 2) else -1
+        trial_meta['start_stim_index'].append(start_idx[pair_index])
+        trial_meta['end_stim_index'].append(end_idx[pair_index])
+        trial_meta['trial_meta_index'].append(pair_index)
+        trial_meta['stim_drift_direction'].append(stim_drift_direction)
+        end_stim_index = end_idx[pair_index]
+        other = np.arange(num_stims)
+        other = other[np.where(other != end_stim_index)]
+        num_options = np.random.choice([2, 4, num_stims]) if num_options is None else num_options
+        options_ixs = np.asarray(np.concatenate(([end_stim_index],
+                                                 np.random.choice(other, num_options - 1, replace=False))), dtype=int)
+        options_pos, options_bin = np.zeros(num_stims), np.zeros(num_stims)
+        options_bin[options_ixs] = 1
+        options_pos[options_ixs] = np.random.permutation(np.arange(num_options))
+        trial_meta['num_options'].append(num_options)
+        trial_meta['options_pos'].append(options_pos)
+        trial_meta['options_bin'].append(options_bin)
+        if fixed_dist is None:
+            stim_dist = np.random.geometric(prob, size=num_stims)
+            stim_dist = np.clip(stim_dist, 1, max_dist)
+            stim_dist_cum = np.cumsum(stim_dist)
+            trial_meta['stim_dist_cum'].append(stim_dist_cum)
+        else:
+            stim_dist = np.repeat([fixed_dist], num_stims)
+            stim_dist_cum = np.cumsum(stim_dist)
+            trial_meta['stim_dist_cum'].append(stim_dist_cum)
+    return trial_meta
--- mental-nav_utils.py
+++ mental-nav_utils.py
@@ -1,22 +1,48 @@
 import pickle as pk
 
-dir_path = "/Users/apiccato/PycharmProjects/mental-navigation-mworks/pickle_meta"
-meta_fn_tag = getvar('trials_from_meta')
+username = 'apiccato'
+# username = 'aidapiccato'
+dir_path = "/Users/%s/PycharmProjects/mental-navigation-mworks/pickle_meta" % username
+meta_fn_tag = getvar('meta_index')
 
-fn = '%s/1.pkl' % dir_path
-meta = pk.load(open(fn, 'rb'))
+seq_fn = "%s/seq_%s.pkl" % (dir_path, meta_fn_tag)
+trial_fn = '%s/trial_%s.pkl' % (dir_path, meta_fn_tag)
+session_fn = '%s/sess_%s.pkl' % (dir_path, meta_fn_tag)
 
-def get_metaparameters():
-	idx = getvar('selection_index')
-	setvar('py_start_stim_index', int(meta['start_stim_index'][idx]))
-	setvar('py_end_stim_index', int(meta['end_stim_index'][idx]))
-	setvar('py_pair_index', int(meta['pair_index'][idx]))
-	setvar('py_meta_index', meta['meta_index'][idx])
-	setvar('py_options_pos', meta['options_pos'][idx])
-	setvar('py_options_bin', meta['options_bin'][idx])
-	setvar('py_num_options', meta['num_options'][idx])
-	setvar('py_stim_drift_direction', meta['stim_drift_direction'][idx])
-	setvar('py_stim_dist_cum', meta['stim_dist_cum'][idx])
+trial_meta = pk.load(open(trial_fn, 'rb'))
+seq_meta = pk.load(open(seq_fn, 'rb'))
+sess_meta = pk.load(open(session_fn, 'rb'))
+
+def get_trial_metaparameters():
+	trial_idx = getvar('selection_trial_index')
+	seq_idx = getvar('selection_seq_index')
+	meta = trial_meta[seq_idx]
+
+	setvar('py_start_stim_index', int(meta['start_stim_index'][trial_idx]))
+	setvar('py_end_stim_index', int(meta['end_stim_index'][trial_idx]))
+	setvar('py_pair_index', int(meta['pair_index'][trial_idx]))
+	setvar('py_meta_trial_index', meta['meta_trial_index'][trial_idx])
+	setvar('py_options_pos', meta['options_pos'][trial_idx])
+	setvar('py_options_bin', meta['options_bin'][trial_idx])
+	setvar('py_num_options', meta['num_options'][trial_idx])
+	setvar('py_stim_drift_direction', meta['stim_drift_direction'][trial_idx])
+	setvar('py_stim_dist_cum', meta['stim_dist_cum'][trial_idx])
 	return
+
+
+def get_seq_metaparameters():
+	id = int(getvar('seq_id'))
+	meta = seq_meta
+	setvar('py_n_stims', int(meta['n_stims'][id]))
+	setvar('py_stim_paths', meta['stim_paths'][id])
+	return
+
+def get_subject_metaparameters():
+	subject_id = getvar('subject_id')
+
+	meta = sess_meta[subject_id]
+	setvar('py_seq_index', int(meta['seq_index']))
+	setvar('py_seqs', meta['seqs'])
+
 
 
